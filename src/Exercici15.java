42c6bf6ca976f888bed666c09f34b3ccde738318
Pau <pauramisnunez@gmail.com>
2022-10-21 16:13:47 +0200

    15real

--- src/Exercici15.java
+++ src/Exercici15.java
@@ -3,6 +3,7 @@ public class Exercici15 {
         //Quantes vegades s'executa el següent bucle?
         int i = 0;
         while (true) {
+            System.out.println(i);
             i++;
             if (i < 10)
                 continue;
@@ -10,7 +11,8 @@ public class Exercici15 {
             if (i == 10)
                 break;
         }
-        //El bucle s'executa 5 vegades, fins que i == 10 i fá un break del bucle infinit.
+        //El bucle s'executa 10 vegades, fins que i deixa de ser més petit que 10,
+        // pero no executa el i==10, perque antes hi ha un i++ que fa que i sigui 11.
+        //Supos que el programa es cancela en aquest moment perque no pot fer res més.
     }
 }
-S
